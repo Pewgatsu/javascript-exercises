1f85fc60a3d98f3b9bda0882d25e8ce50ac55cd5
james <jamesdeleon373@gmail.com>
2023-03-23 21:52:48 +0800

    Finished exercise 5 and 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,42 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    /*
+
+    Leap years -> divisible by 4,
+    Not Leap years -> divisible by 100 unless they are divisible by 400
+
+    
+    check if year is divisible by 400
+    if yes, then it is a leap year
+    if not, check if it is divisible by 100,
+    if yes, then it is not a leap year,
+
+    1600 -> divisble 4, 400, 100
+
+    */
+
+    if ( year % 4 === 0) {
+
+        if ( year % 400 === 0 && year % 100 === 0 ) {
+            return true;
+        }
+
+        if ( !year % 400 === 0 && year % 100 === 0) {
+            return false;
+        }
+
+        return true;
+        
+    }else {
+        return false;
+    }
+
+
+
+
+
+    
+
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,36 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+
+  /*
+    multiply by 5/9 -32
+
+  */
+
+  let result = 0;
+
+
+  result = (temperature - 32) * 5/9;
+
+
+  return parseFloat(result.toFixed(1));
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+
+  let result = 0;
+
+
+  /*
+     multiply by 9/5 + 32
+  */
+
+   result = (temperature * 9/5) + 32;
+
+
+   return parseFloat(result.toFixed(1));
+  
+
 };
 
 // Do not edit below this line
