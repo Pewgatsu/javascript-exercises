d61119a606a3a56972758f09df39792dc0adcb1a
james <jamesdeleon373@gmail.com>
2023-04-06 23:29:30 +0800

    Finished palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+
+    
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    
+    let start = 0;
+    let end = word.length - 1;
+
+    while (start < end ){
+        
+        if (word[start] !== word[end]){
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
+
+    
+
 
 };
 
